857f6aa35dea205de056b49fd5299e791a8cdefc
Hesam eddin Hashemi <109273285+hesamhme@users.noreply.github.com>
2023-02-20 00:54:16 +0330

    detail view created

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ProductsListView
+from .views import ProductsListView, ProductDetails
 
 urlpatterns = [
     path('', ProductsListView.as_view(), name='list_view'),
+    path('<int:pk>/', ProductDetails.as_view(), name='detail_view'),
+
+
 
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,12 @@ from .models import Products
 
 
 class ProductsListView(generic.ListView):
-    queryset = Products.objects.filter('active')
-    # model = Products
+    # queryset = Products.objects.filter('active')
+    model = Products
     template_name = 'products/procust_list_view.html'
+
+
+class ProductDetails(generic.DetailView):
+    model = Products
+    template_name = 'products/products_detail_view.html'
+    context_object_name = 'pro'
