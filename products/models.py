578518e00ed772bcbb38c12022df0e8af810e8e7
Hesam eddin Hashemi <109273285+hesamhme@users.noreply.github.com>
2023-02-20 20:49:16 +0330

    comments model created

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 
 class Products(models.Model):
@@ -15,5 +16,22 @@ class Products(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail_view', args=[self.pk])
+
+
+class Comment(models.Model):
+    POINTS = [
+        ('1', 'very bad'),
+        ('2', 'bad'),
+        ('3', 'normal'),
+        ('4', 'good'),
+        ('5', 'perfect'),
+    ]
+    products = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    stars = models.CharField(max_length=10, choices=POINTS)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
 
