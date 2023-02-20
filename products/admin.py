56028ef12ecb08e37da8a70474ad87d6f57f24c1
Hesam eddin Hashemi <109273285+hesamhme@users.noreply.github.com>
2023-02-20 21:23:14 +0330

    comment admin created

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products, Comment
 
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active']
 
+
+@admin.register(Comment)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['author', 'stars', 'products', 'active']
