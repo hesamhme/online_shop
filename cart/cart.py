cda351663588530056b35506916d3c5cb241af32
Hesam eddin Hashemi <109273285+hesamhme@users.noreply.github.com>
2023-02-22 23:04:20 +0330

    quantity add, fixed

--- cart/cart.py
+++ cart/cart.py
@@ -12,3 +12,14 @@ class Cart:
 
         self.cart = cart
 
+    def add(self, product, quantity=1):
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': quantity}
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        self.session.modified = True
